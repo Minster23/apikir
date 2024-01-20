cd302f0da1ec16dd1d731cf4d5b6fe6373cd619b
Whisker <100831738+Minster23@users.noreply.github.com>
2024-01-20 14:31:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
