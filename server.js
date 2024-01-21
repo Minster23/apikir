c5125fce430849924e0b8b6ccfa33156d594c1fa
Whisker <100831738+Minster23@users.noreply.github.com>
2024-01-21 15:19:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({origin:"http://localhost:5173"}));
 app.use(express.json());
 app.use(bodyParser.json());
 
